d019ccc05f9aac61027e62ff8d6d9781ebf35902
basosa94 <basosa@ucdavis.edu>
2018-04-30 20:59:01 -0700

    Fix css file so that the images are aligning in the center and add pseudocode to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,21 @@
+// Define variables
 var counter=0;
 var wins=0;
 var losses=0;
 
-var targetNumber=Math.floor(Math.random()*101)+19;
+// Assign targetNumber variable for our guess
+var targetNumber=Math.floor(Math.random()*102)+19;
 
+// Assign initial scores to html page
 $("#numGuess").text(targetNumber);
 $("#currentNum").text(counter);
 $("#winCounter").text(wins);
 $("#lossCounter").text(losses);
 
-var numberOptions =[Math.floor(Math.random()*11)+1];
+// Make array of images for crystals
 var imageLinks=["assets/images/Steven_Shield_WD.png", "assets/images/Current_Pearl_Request.png", "assets/images/GarnetByKmes.png", "assets/images/Jfek_New_Gen.png"];
 
+// Make for-loop that will assign random value from 1-12 to each of our images
 for (var i= 0; i< imageLinks.length; i++) {
     var imageCrystal = $("<img>");
 
@@ -19,11 +23,12 @@ for (var i= 0; i< imageLinks.length; i++) {
 
     imageCrystal.attr("src", imageLinks[i]);
 
-    imageCrystal.attr("data-crystalvalue", Math.floor(Math.random()*11)+1);
+    imageCrystal.attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
 
     $("#crystals").append(imageCrystal);
 }
 
+// Main Process
 $(".crystal-image").on("click", function(){
 
     var crystalValue = ($(this).attr("data-crystalvalue"));
